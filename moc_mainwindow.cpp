495176ae17f4cb9ed6b901b702d4135492da58a9
TwilightSparkleX <venompop@gmail.com>
2019-01-14 15:02:15 +0300

    First version 1.0

---
+++ moc_mainwindow.cpp
@@ -0,0 +1,236 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.3.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../WMS_Emulator/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.3.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[28];
+    char stringdata[473];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10),
+QT_MOC_LITERAL(1, 11, 7),
+QT_MOC_LITERAL(2, 19, 0),
+QT_MOC_LITERAL(3, 20, 26),
+QT_MOC_LITERAL(4, 47, 27),
+QT_MOC_LITERAL(5, 75, 7),
+QT_MOC_LITERAL(6, 83, 22),
+QT_MOC_LITERAL(7, 106, 27),
+QT_MOC_LITERAL(8, 134, 8),
+QT_MOC_LITERAL(9, 143, 14),
+QT_MOC_LITERAL(10, 158, 5),
+QT_MOC_LITERAL(11, 164, 21),
+QT_MOC_LITERAL(12, 186, 23),
+QT_MOC_LITERAL(13, 210, 25),
+QT_MOC_LITERAL(14, 236, 30),
+QT_MOC_LITERAL(15, 267, 30),
+QT_MOC_LITERAL(16, 298, 13),
+QT_MOC_LITERAL(17, 312, 11),
+QT_MOC_LITERAL(18, 324, 11),
+QT_MOC_LITERAL(19, 336, 20),
+QT_MOC_LITERAL(20, 357, 21),
+QT_MOC_LITERAL(21, 379, 4),
+QT_MOC_LITERAL(22, 384, 23),
+QT_MOC_LITERAL(23, 408, 10),
+QT_MOC_LITERAL(24, 419, 4),
+QT_MOC_LITERAL(25, 424, 7),
+QT_MOC_LITERAL(26, 432, 16),
+QT_MOC_LITERAL(27, 449, 23)
+    },
+    "MainWindow\0onReady\0\0on_put2lightButton_clicked\0"
+    "on_pick2lightButton_clicked\0product\0"
+    "on_StartButton_clicked\0"
+    "on_AddProductButton_clicked\0onResult\0"
+    "QNetworkReply*\0reply\0on_ScanButton_clicked\0"
+    "on_textEdit_textChanged\0"
+    "on_AddImageButton_clicked\0"
+    "on_ChangeProductButton_clicked\0"
+    "on_DeleteProductButton_clicked\0"
+    "newConnection\0isconnected\0pickproduct\0"
+    "on_addToList_clicked\0on_comboBox_activated\0"
+    "arg1\0on_ConfigWindow_clicked\0changeText\0"
+    "text\0portvar\0on_SetIP_clicked\0"
+    "on_DeleteButton_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      21,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,  119,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       3,    0,  120,    2, 0x08 /* Private */,
+       4,    0,  121,    2, 0x08 /* Private */,
+       5,    0,  122,    2, 0x08 /* Private */,
+       6,    0,  123,    2, 0x08 /* Private */,
+       7,    0,  124,    2, 0x08 /* Private */,
+       8,    1,  125,    2, 0x08 /* Private */,
+      11,    0,  128,    2, 0x08 /* Private */,
+      12,    0,  129,    2, 0x08 /* Private */,
+      13,    0,  130,    2, 0x08 /* Private */,
+      14,    0,  131,    2, 0x08 /* Private */,
+      15,    0,  132,    2, 0x08 /* Private */,
+      16,    0,  133,    2, 0x08 /* Private */,
+      17,    0,  134,    2, 0x08 /* Private */,
+      18,    0,  135,    2, 0x08 /* Private */,
+      19,    0,  136,    2, 0x08 /* Private */,
+      20,    1,  137,    2, 0x08 /* Private */,
+      22,    0,  140,    2, 0x08 /* Private */,
+      23,    2,  141,    2, 0x08 /* Private */,
+      26,    0,  146,    2, 0x08 /* Private */,
+      27,    0,  147,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 9,   10,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   21,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString, QMetaType::Int,   24,   25,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        switch (_id) {
+        case 0: _t->onReady(); break;
+        case 1: _t->on_put2lightButton_clicked(); break;
+        case 2: _t->on_pick2lightButton_clicked(); break;
+        case 3: _t->product(); break;
+        case 4: _t->on_StartButton_clicked(); break;
+        case 5: _t->on_AddProductButton_clicked(); break;
+        case 6: _t->onResult((*reinterpret_cast< QNetworkReply*(*)>(_a[1]))); break;
+        case 7: _t->on_ScanButton_clicked(); break;
+        case 8: _t->on_textEdit_textChanged(); break;
+        case 9: _t->on_AddImageButton_clicked(); break;
+        case 10: _t->on_ChangeProductButton_clicked(); break;
+        case 11: _t->on_DeleteProductButton_clicked(); break;
+        case 12: _t->newConnection(); break;
+        case 13: _t->isconnected(); break;
+        case 14: _t->pickproduct(); break;
+        case 15: _t->on_addToList_clicked(); break;
+        case 16: _t->on_comboBox_activated((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 17: _t->on_ConfigWindow_clicked(); break;
+        case 18: _t->changeText((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< qint32(*)>(_a[2]))); break;
+        case 19: _t->on_SetIP_clicked(); break;
+        case 20: _t->on_DeleteButton_clicked(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 6:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QNetworkReply* >(); break;
+            }
+            break;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (MainWindow::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&MainWindow::onReady)) {
+                *result = 0;
+            }
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata))
+        return static_cast<void*>(const_cast< MainWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 21)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 21;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 21)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 21;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void MainWindow::onReady()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, 0);
+}
+QT_END_MOC_NAMESPACE
